538d78047f4797a7bb21c4ea537a197923d5190d
Ryan Kim <ryanhkim0418@gmail.com>
2025-07-08 19:15:43 -0500

    Adding dialog and functionalities for buttons

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const myLibrary = [];
 
+const showButton = document.getElementById("showDialog");
+const newBook = document.getElementById("newBook");
+const tempName = newBook.querySelector("name");
+const tempAuthor = newBook.querySelector("author");
+const tempPages = newBook.querySelector("pages");
+const confirmBtn = newBook.querySelector("#confirmBtn");
+
 function Book(name, author, pages) {
   // the constructor...
   this.id = crypto.randomUUID();
@@ -17,22 +24,36 @@ function Book(name, author, pages) {
   
 }
 
+function addCard(cardContainer, book){
+    const bookDiv = document.createElement("div");
+    const removeButton = document.createElement("button");
+
+    bookDiv.classList.add("card");
+    bookDiv.textContent = book.info();
+    if(book.read){
+        bookDiv.style.boxShadow = "-5px 5px #82b74b";
+    }
+
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("remove");
+    bookDiv.appendChild(removeButton);
+
+    cardContainer.appendChild(bookDiv);
+}
+
 function addBookToLibrary(Book) {
   // take params, create a book then store it in the array
-    myLibrary.push(Book);
+    myLibrary.unshift(Book);
 }
 
 function showLibrary(){
-    const cardContainer = document.querySelector(".card-container")
+    const cardContainer = document.querySelector(".card-container");
+    cardContainer.replaceChildren();
     for(let i = 0; i < myLibrary.length; i++){
-        console.log(myLibrary[i].info());
-        const bookDiv = document.createElement("div");
-        bookDiv.classList.add("card");
-        bookDiv.textContent = myLibrary[i].info();
-        if(myLibrary[i].read){
-            bookDiv.style.boxShadow = "-5px 5px #82b74b";
+        if(myLibrary[i].name !== null){
+            //console.log(myLibrary[i].info());
+            addCard(cardContainer, myLibrary[i]);
         }
-        cardContainer.appendChild(bookDiv);
     }
 }
 
@@ -49,3 +70,21 @@ addBookToLibrary(bookThree);
 addBookToLibrary(bookFour);
 
 showLibrary();
+
+showButton.addEventListener("click", () => {
+  newBook.showModal();
+});
+
+newBook.addEventListener("close", (e) => {
+    e.preventDefault();
+    dialog.close();
+});
+
+newBook.addEventListener("click", (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  console.log("selected!");
+  let newBook = new Book(tempName, tempAuthor, tempPages);
+  addBookToLibrary(newBook);
+  showLibrary();
+  dialog.close();
+});
